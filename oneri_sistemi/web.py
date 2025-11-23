6cdbc18d9ea89c7a520b573028204753f73b9d3b
kocatekin <116370462+kocatekin@users.noreply.github.com>
2025-11-23 19:58:29 +0300

    Update web.py

--- oneri_sistemi/web.py
+++ oneri_sistemi/web.py
@@ -1,29 +1,35 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-import requests as req
-
-from use import run
+from use import run_local_model
+from flask import Flask, jsonify, request, send_from_directory
 
 app = Flask(__name__)
-CORS(app)
 
-OLLAMA_URL = "http://localhost:11434/api/generate"
+SYSTEM_PROMPT = """
+"""
+USER_PROMPT = """"""
 
-@app.route('/generate', methods=['POST'])
+@app.route("/")
+def index():
+    return send_from_directory("static","index.html")
+
+
+@app.route("/llm", methods=['POST'])
 def generate():
-   data = request.json
-   print(data) #this is a json with prompt
-   if not data or "prompt" not in data:
-      return jsonify({"error":"missing prompt"}), 400
-   
-   try:
-      print(data['prompt'])
-      ans = run(data['prompt'])
-      print(ans)
-      return jsonify(ans)
-   except Exception as e:
-      return jsonify({"error": str(e)}), 500
-   
-if __name__ == "__main__":
-   app.run(port=5000)
+    data = request.json
+    print(data)
+    if not data:
+        return jsonify({"error":"no json body recevied"}), 400
 
+    # tones here
+    system_prompt = f'You are an excuse generator. You are a very {data["toSend"]["tone"]} person. Create a {data["toSend"]["tone"]} response. Just give the response. Make no explanations.'
+    user_prompt = data["toSend"]["topic"]
+    
+    try:
+        print("sending to local llm model")
+        ans = run_local_model(system_prompt, user_prompt)
+        print(ans)
+        return jsonify(ans)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
